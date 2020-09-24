79c67ff9c0024b6a98f41e7751487c9e6949f247
brunomeloesilva <brunomeloesilva@gmail.com>
2020-09-24 15:02:21 -0400

    Estilizando o componente atraves de um  .css

---
+++ src/components/App/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./App.css";
+
+const App = () => <div className="App">todo: home view</div>
+
+export default App
--- src/views/Home.view.jsx
+++ src/views/Home.view.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import App from "../components/App/App";
 
-const HomeView = () => <div>todo: home view</div>
+const HomeView = () => <div><App/></div>
 
 export default HomeView
