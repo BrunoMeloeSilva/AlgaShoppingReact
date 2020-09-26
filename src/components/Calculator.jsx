a876bde31f90086fb4ae8d19932eef261abd3179
brunomeloesilva <brunomeloesilva@gmail.com>
2020-09-25 20:18:52 -0400

    Ajuste final do uso de redux

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { sum } from "../store/Calculator/Calculator.actions";
+import { subtract, sum } from "../store/Calculator/Calculator.actions";
 
 function Calculator() {
     
     const dispatch = useDispatch()
     const result = useSelector(state => state.calculator)
+    const [a, setA] = useState(0)
+    const [b, setB] = useState(0)
 
     return <>
-        <input type="text" placeholder="Informe valor a"/>
-        <input type="text" placeholder="Informe valor b"/>
-        <button onClick = {() => { dispatch(sum(1,5)) }}>Somar</button>
+        <input type="text" placeholder="Informe valor a" onChange = {e => setA(e.target.value)}/>
+        <input type="text" placeholder="Informe valor b" onChange = {e => setB(e.target.value)}/>
+        <button onClick = {() => { dispatch(sum(a, b)) }}>Somar</button>
+        <button onClick = {() => { dispatch(subtract(a, b)) }}>Subtrair</button>
         <div>Resultado: {result}</div>
     </>
 }
--- src/store/Calculator/Calculator.reducer.js
+++ src/store/Calculator/Calculator.reducer.js
@@ -1,7 +1,7 @@
 export default function(state = 0, action) {
     switch (action.type) {
         case 'SUM':
-            return action.payload[0] + action.payload[1]
+            return Number(action.payload[0]) + Number(action.payload[1])
         case 'SUBTRACT':
             return action.payload[0] - action.payload[1]
         default:
